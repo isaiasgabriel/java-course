053d4f384a075da6190f0ee5a3605f5d4f45f11f
Isaías Gabriel <is.gabri3l@gmail.com>
2025-06-09 16:08:31 -0300

    FileWriter & BufferedWriter

---
+++ 8. Files/3. FileWriter & BufferedWriter/src/App.java
@@ -0,0 +1,21 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String[] lines = new String[] { "Good morning", "Good evening", "Good night" };
+        String path = "/home/user/java-workplace/curso/8. Files/3. FileWriter & BufferedWriter/out.txt";
+
+        // new FileWriter(path, true); -> append = true 
+        // add new lines without replacing everything
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("ERROR: " + e.getMessage());
+        }
+    }
+}
